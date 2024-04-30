b263fb8d54a7e9d1bc16e547d7adfb4e88e9b9ed
s-shiroishi <138426968+s-shiroishi@users.noreply.github.com>
2024-04-30 20:34:26 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,41 +1,41 @@
-import React, {useState, useRef} from "react";
-import {v4 as uuid} from 'uuid';
+import React, { useState, useRef } from "react";
+import { v4 as uuid } from 'uuid';
 
 function App() {
-  const inputRef= useRef('');
+  const inputRef = useRef('');
   const [taskList, setTaskList] = useState([]);
   const [filterCondition, setFilterCondition] = useState('すべて');
 
   const clickAddHandler = () => {
     const newTaskList = [...taskList];
-    newTaskList.push({id: uuid(), comment: inputRef.current.value, condition: '作業中'});
+    newTaskList.push({ id: uuid(), comment: inputRef.current.value, condition: '作業中' });
     setTaskList(newTaskList);
     inputRef.current.value = '';
   }
 
   const clickConditionHandler = (e) => {
     setTaskList(taskList.map((task) => {
-      if(task.id===e.target.value){
-        return {...task, condition: task.condition === '作業中' ? '完了': '作業中'};
-      }else{
+      if (task.id === e.target.value) {
+        return { ...task, condition: task.condition === '作業中' ? '完了' : '作業中' };
+      } else {
         return task;
       }
-      
+
     }))
   }
 
   const clickDeleteHandler = (e) => {
     setTaskList(taskList.filter((task) => task.id !== e.target.value));
   }
-  
+
   const changeConditionHandler = (e) => {
     setFilterCondition(e.target.value);
   }
 
   const getViewTask = () => {
-    if(filterCondition === "すべて"){
+    if (filterCondition === "すべて") {
       return taskList;
-    }else{
+    } else {
       return taskList.filter((task) => task.condition === filterCondition);
     }
   }
@@ -63,14 +63,14 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {getViewTask().map((task,id)=>(
-                <tr key={task.id}>
-                  <td>{id}</td>
-                  <td>{task.comment}</td>
-                  <td><button value={task.id} onClick={clickConditionHandler}>{task.condition}</button></td>
-                  <td><button value={task.id} onClick={clickDeleteHandler}>削除</button></td>
-                </tr>
-              )
+            {getViewTask().map((task, id) => (
+              <tr key={task.id}>
+                <td>{id}</td>
+                <td>{task.comment}</td>
+                <td><button value={task.id} onClick={clickConditionHandler}>{task.condition}</button></td>
+                <td><button value={task.id} onClick={clickDeleteHandler}>削除</button></td>
+              </tr>
+            )
             )}
           </tbody>
         </table>
@@ -82,4 +82,3 @@ function App() {
 }
 
 export default App;
-
